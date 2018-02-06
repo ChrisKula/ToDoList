52ca93706d752622593d03695373373702a75f10
Christian KULA <christian.kula.ck@gmail.com>
2018-02-06 10:56:00 +0100

    Wire up Dagger modules & component for ToDoList

--- app/src/main/java/com/christiankula/todolist/ToDoListApplication.java
+++ app/src/main/java/com/christiankula/todolist/ToDoListApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.christiankula.todolist.injection.components.DaggerToDoListComponent;
 import com.christiankula.todolist.injection.components.ToDoListComponent;
+import com.christiankula.todolist.injection.modules.ToDoListModule;
 
 public class ToDoListApplication extends Application {
 
@@ -12,9 +13,14 @@ public class ToDoListApplication extends Application {
     protected ToDoListComponent createComponent() {
         if (component == null) {
             return DaggerToDoListComponent.builder()
+                    .toDoListModule(new ToDoListModule())
                     .build();
         } else {
             throw new IllegalStateException("You can't recreate a component for ToDoListApplication");
         }
+    }
+
+    public ToDoListComponent getComponent() {
+        return component;
     }
 }
--- app/src/main/java/com/christiankula/todolist/todolist/ToDoListActivity.java
+++ app/src/main/java/com/christiankula/todolist/todolist/ToDoListActivity.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.christiankula.todolist.R;
+import com.christiankula.todolist.ToDoListApplication;
 import com.christiankula.todolist.todolist.mvp.ToDoListMvp;
+
+import javax.inject.Inject;
 
 public class ToDoListActivity extends AppCompatActivity implements ToDoListMvp.View {
 
@@ -14,8 +17,11 @@ public class ToDoListActivity extends AppCompatActivity implements ToDoListMvp.V
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todolist);
+
+        ((ToDoListApplication) getApplication()).getComponent().inject(this);
     }
 
+    @Inject
     @Override
     public void setPresenter(ToDoListMvp.Presenter presenter) {
         this.presenter = presenter;
