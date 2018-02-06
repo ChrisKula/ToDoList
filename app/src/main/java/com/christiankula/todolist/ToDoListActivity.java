4fa35c90f4d2dae50087ac93e50705646bbf9f6a
Christian KULA <christian.kula.ck@gmail.com>
2018-02-06 10:28:44 +0100

    Renamed MainActivity to ToDoListActivity

--- app/src/main/java/com/christiankula/todolist/ToDoListActivity.java
+++ app/src/main/java/com/christiankula/todolist/ToDoListActivity.java
@@ -1,9 +1,9 @@
 package com.christiankula.todolist;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class ToDoListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
