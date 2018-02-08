f46f47ff30b3d233dddf65ab0f450a65df282bf2
Christian KULA <christian.kula.ck@gmail.com>
2018-02-08 12:51:04 +0100

    Implement New ToDo activity

--- app/src/main/java/com/christiankula/todolist/newtodo/NewToDoModel.java
+++ app/src/main/java/com/christiankula/todolist/newtodo/NewToDoModel.java
@@ -1,5 +1,42 @@
 package com.christiankula.todolist.newtodo;
 
+import com.christiankula.todolist.models.ToDo;
+
+import java.util.Calendar;
+
 public class NewToDoModel implements NewToDoMvp.Model {
 
+    private String toDoDescription;
+    private Calendar toDoDateTime;
+
+
+    public NewToDoModel() {
+        this.toDoDescription = "";
+        this.toDoDateTime = Calendar.getInstance();
+    }
+
+    @Override
+    public void setToDoDateTime(Calendar toDoDateTime) {
+        this.toDoDateTime = toDoDateTime;
+    }
+
+    @Override
+    public Calendar getToDoDateTime() {
+        return (Calendar) toDoDateTime.clone();
+    }
+
+    @Override
+    public void setToDoDescription(String description) {
+        this.toDoDescription = description;
+    }
+
+    @Override
+    public String getToDoDescription() {
+        return toDoDescription;
+    }
+
+    @Override
+    public void saveToDo(ToDo toDo) {
+        //TODO implement saving
+    }
 }
